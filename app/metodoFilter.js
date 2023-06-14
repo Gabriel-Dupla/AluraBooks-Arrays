7025d220bb9fd7a4332fa97a7420675ae5c5bbeb
Gabriel-Dupla <gabrielduplacontato@gmail.com>
2023-06-14 14:18:38 -0300

    aula5 filtrando e aparecendo valor total

--- app/metodoFilter.js
+++ app/metodoFilter.js
@@ -6,13 +6,34 @@ function filtrarLivros() {
     const elementoBtn = document.getElementById(this.id)
     const categoria = elementoBtn.value
     //pegamos o id do elemento clicado e depois o seu valor como categoria
-    let livrosFiltrados = categoria == 'disponivel' ? livros.filter(livro => livro.quantidade) : livros.filter(livro => livro.categoria == categoria)
+    let livrosFiltrados = categoria == 'disponivel' ? filtrarPorDisponibilidade() : filtrarPorCategoria(categoria)
     //armazenamos os livros em uma variável
     //todo livro especifico vai ter uma ação específica (=>). Pegamos todos os livros com a categoria front-end, filtrando esse conteúdo e armazenando
     exibirOsLivrosNaTela(livrosFiltrados)
-
-
+    if (categoria == 'disponivel') {
+        const valorTotal = calcularValorTotalDeLivrosDisponiveis(livrosFiltrados)
+        exibirValorTotalDosLivrosDisponiveisNaTela(valorTotal)
+    }
 }
+
+
+function FiltrarPorCategoria(categoria) {
+    return livros.filter(livro => livro.categoria == categoria)
+}
+
+function filtrarPorDisponibilidade() {
+    return livros.filter(livro => livro.quantidade > 0)
+}
+
+function exibirValorTotalDosLivrosDisponiveisNaTela(valorTotal) {
+    elementoComValorTotalDeLivrosDisponiveis.innerHTML = `
+    <div class="livros__disponiveis">
+    <p>Todos os livros disponíveis por R$ <span id="valor">${valorTotal}</span></p>
+    </div>
+    `
+}
+
+
 
 //Antigo código apenas para livros de front
 
