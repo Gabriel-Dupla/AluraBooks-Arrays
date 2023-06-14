ff2a7ec68c4e70651617d95f97f26dde1c641ea5
Gabriel-Dupla <gabrielduplacontato@gmail.com>
2023-06-14 12:51:22 -0300

    Aula4 filter disponiveis com defeito na api

--- app/metodoFilter.js
+++ app/metodoFilter.js
@@ -6,7 +6,7 @@ function filtrarLivros() {
     const elementoBtn = document.getElementById(this.id)
     const categoria = elementoBtn.value
     //pegamos o id do elemento clicado e depois o seu valor como categoria
-    let livrosFiltrados = livros.filter(livro => livro.categoria == categoria)
+    let livrosFiltrados = categoria == 'disponivel' ? livros.filter(livro => livro.quantidade) : livros.filter(livro => livro.categoria == categoria)
     //armazenamos os livros em uma variável
     //todo livro especifico vai ter uma ação específica (=>). Pegamos todos os livros com a categoria front-end, filtrando esse conteúdo e armazenando
     exibirOsLivrosNaTela(livrosFiltrados)
---
+++ app/metodoSort.js
@@ -0,0 +1,8 @@
+let btnOrdenarPorPreco = document.getElementById('')
+btnOrdenarPorPreco.addEventListener('click', ordenarLivrosPorPreco)
+
+function ordenarLivrosPorPreco() { 
+    let livrosOrdenados = livros.sort((a, b) => a.preco - b.preco)
+    exibirOsLivrosNaTela(livrosOrdenados)
+
+}
